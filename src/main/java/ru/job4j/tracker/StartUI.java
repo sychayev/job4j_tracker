0e69a21886c8cb5e97bc8186e6b97233b17c2236
petrs <sychayev.petr>
2020-09-29 20:06:30 +0300

    5. Tracker - хранилище

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 public class StartUI {
+
     public static void main(String[] args) {
         Item i = new Item();
         Tracker track = new Tracker();
@@ -9,3 +10,4 @@ public class StartUI {
 
     }
 }
+
