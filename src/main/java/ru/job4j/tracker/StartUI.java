b66c21cb1f783620d913fea01d022a4421325814
petrs <sychayev.petr>
2020-09-29 14:15:19 +0300

    5. Tracker - хранилище

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,11 @@
+package ru.job4j.tracker;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item i = new Item();
+        Tracker track = new Tracker();
+        track.add(i);
+        System.out.println(track.findById(3));
+
+    }
+}
--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -14,4 +14,26 @@ public class TrackerTest {
         Item result = tracker.findById(item.getId());
         assertThat(result.getName(), is(item.getName()));
     }
+    @Test
+    public void whenFindByName() {
+        Tracker track = new Tracker();
+        Item it = new Item();
+        it.setName("A");
+        track.add(it);
+        String key = it.getName();
+        track.findByName(key);
+        assertThat(track.findByName(key)[0].getName(),is("A"));
+    }
+    @Test
+    public void whenReplace() {
+        Tracker tracker = new Tracker();
+        Item bug = new Item();
+        bug.setName("Bug");
+        tracker.add(bug);
+        int id = bug.getId();
+        Item bugWithDesc = new Item();
+        bugWithDesc.setName("Bug with description");
+//        tracker.replace(id, bugWithDesc);
+        assertThat(tracker.findById(id).getName(), is("Bug with description"));
+    }
 }
