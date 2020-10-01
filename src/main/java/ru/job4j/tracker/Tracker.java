32bb33ccb50b5112b085f29d37f6c2d07c945d34
petrs <sychayev.petr>
2020-10-01 20:26:02 +0300

    8. Что такое валидация?

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Tracker {
 
-    public Item[] items = new Item[100];
+    private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
 
@@ -95,4 +95,5 @@ public class Tracker {
 //        }
 //        return res;
 //    }
+
 }
