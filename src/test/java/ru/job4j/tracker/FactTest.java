dbf9ea237788282059a15f589f70fb4d010e8814
petrs <sychayev.petr>
2020-10-16 14:14:07 +0300

    0.3. Тестирование исключений с junit

--- src/test/java/ru/job4j/tracker/FactTest.java
+++ src/test/java/ru/job4j/tracker/FactTest.java
@@ -6,6 +6,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class FactTest {
+
     @Test(expected = IllegalArgumentException.class)
     public void whenTestEx() {
         Fact.calc(-5);
