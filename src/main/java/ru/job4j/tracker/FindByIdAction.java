25ad0dadff354c891193722cd3eab0ef365ca06d
petrs <sychayev.petr>
2020-10-16 14:09:30 +0300

    9.1 Зависимость от System.out

--- src/main/java/ru/job4j/tracker/FindByIdAction.java
+++ src/main/java/ru/job4j/tracker/FindByIdAction.java
@@ -1,18 +1,27 @@
 package ru.job4j.tracker;
 
-public class FindByIdAction implements UserAction{
+public class FindByIdAction implements UserAction {
     private final Output out;
+
     public FindByIdAction(Output out) {
         this.out = out;
     }
+
     @Override
     public String name() {
-        return "Find all action items";
+        return "Find item by id!";
     }
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
 
+        int id = input.askInt("Enter id");
+        Item item = tracker.findById(id);
+        if (item != null) {
+            out.println(item);
+        } else {
+            out.println("There is not item with id like this");
+        }
         return true;
     }
 }
