a23da6df2eb18419e568141b62a54d63baa75f77
petrs <sychayev.petr>
2020-10-21 14:38:43 +0300

    9.2. Тесты вывода на консоль

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -151,41 +151,28 @@ public class StartUITest {
                 ("1.Exit") + s));
     }
 
-//    @Test
-//    public void whenAddItem() {
-//        String[] answers = {"Fix PC"};
-//        Input input = new StubInput(answers);
-//        Tracker tracker = new Tracker();
-//        StartUI.creteItem(input, tracker);
-//        Item created = tracker.findAll()[0];
-//        Item expected = new Item("Fix PC");
-//        assertThat(created.getName(), is(expected));
-//    }
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
 
-//    @Test
-//    public void whenReplaceItem() {
-//        Tracker tracker = new Tracker();
-//        Item item = new Item("new item");
-//        tracker.add(item);
-//        String[] answers = {
-//                String.valueOf(item.getId()), String.valueOf("C")
-//        };
-//        StartUI.replaceItem(new StubInput(answers), tracker);
-//        Item replaced = tracker.findById(item.getId());
-//        assertThat(replaced.getName(), is("C"));
-//    }
+        Input in = new StubInput(
+                new String[]{"0","1"}
+        );
+        UserAction[] actions = {
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu.%n"
+                                + "0. Exit%n"
+                                + "Wrong input, you can select: 0 .. 0%n"
+                                + "Menu.%n"
+                                + "0. Exit%n"
+                )
+        ));
+    }
 
-//    @Test
-//    public void whenDeleteItem() {
-//        Tracker tracker = new Tracker();
-//        Item item = new Item("new item");
-//        tracker.add(item);
-//        String[] deletedItems = {
-//                String.valueOf(item.getId())
-//        };
-//        StartUI.deleteItem(new StubInput(deletedItems), tracker);
-//        Item delet = tracker.findById(item.getId());
-//        assertThat(delet, is(nullValue()));
-//    }
 }
 
